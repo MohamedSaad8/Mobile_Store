f90b1dd308be24cb3be38e448434092a0510a8be
mohamed saad <mohamedsaad.cis@gmail.com>
2020-09-10 11:34:39 +0200

    advanced search recoded

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -235,69 +235,80 @@ class DataSearch extends SearchDelegate
         else
         {
           return ListView.builder(itemBuilder: (context , index){
-            return Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: InkWell(
-                onTap: ()
-                {
-                  Navigator.pushNamed(context, Details.id,arguments: snapshot.data[index]);
-                },
-                child: Container(
-                  height: MediaQuery.of(context).size.height * .2,
-                  width: double.infinity,
-                  child: Card(
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(
-                          flex: 1,
-                          child: Image(
-                            image: NetworkImage(snapshot.data[index].url),
-                          ),
-                        ),
-                        SizedBox(width: 10,),
-                        Expanded(
-                          flex: 2,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(snapshot.data[index].name .toUpperCase() ,
-                                style: TextStyle(
-                                    fontFamily: "Cairo" ,
-                                    fontSize: 20 ,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.black
-                                ),
+            if(snapshot.data[index].name.toLowerCase() == query.toLowerCase())
+              {
+                return Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: InkWell(
+                    onTap: ()
+                    {
+                      Navigator.pushNamed(context, Details.id,arguments: snapshot.data[index]);
+                    //  snapshot.data[index].name = "this is a name must not found in mobile list" ;
+                      snapshot.data.removeAt(index);
+                    },
+                    child: Container(
+                      height: MediaQuery.of(context).size.height * .2,
+                      width: double.infinity,
+                      child: Card(
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Image(
+                                image:  NetworkImage(snapshot.data[index].url),
                               ),
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
+                            ),
+                            SizedBox(width: 10,),
+                            Expanded(
+                              flex: 2,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: <Widget>[
-                                  Expanded(child: Text("الكاميرا : ${snapshot.data[index].camera}",)),
-                                  Expanded(child: Text("المعالج : ${snapshot.data[index].cpu}",)),
+                                  Text( snapshot.data[index].name .toUpperCase()  ,
+                                    style: TextStyle(
+                                        fontFamily: "Cairo" ,
+                                        fontSize: 20 ,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: <Widget>[
+                                      Expanded(child: Text("الكاميرا : ${snapshot.data[index].camera }",)),
+                                      Expanded(child: Text("المعالج : ${snapshot.data[index].cpu }",)),
 
+                                    ],
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 5),
+                                    child: Row(
+                                      children: <Widget>[
+                                        Expanded(child: Text("البطارية : ${snapshot.data[index].battery}",)),
+                                        Expanded(child: Text("اتلذاكرة : ${snapshot.data[index].memory}",)),
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 5),
+                                    child: Text("السعر : ${snapshot.data[index].price}",),
+                                  ),
                                 ],
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Row(
-                                  children: <Widget>[
-                                    Expanded(child: Text("البطارية : ${snapshot.data[index].battery}",)),
-                                    Expanded(child: Text("اتلذاكرة : ${snapshot.data[index].memory}",)),
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 5),
-                                child: Text("السعر : ${snapshot.data[index].price}",),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
+                            )
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-            );
+                );
+              }
+            else
+              {
+                return Container() ;
+              }
+
+
           } ,
             itemCount: snapshot.data.length,
           );
@@ -316,7 +327,7 @@ class DataSearch extends SearchDelegate
       }
     else
       {
-        searchList =  mobiles.where((p)=> p.startsWith(query)).toList() ;
+        searchList =  mobiles.where((p)=> p.startsWith(query.toLowerCase())).toList() ;
       }
     return ListView.builder(
       itemBuilder: (context , index){
