3e6ddc33ef2ac83f4e6d725c7990698758d9810e
mohamed saad <mohamedsaad.cis@gmail.com>
2020-09-24 19:53:45 +0200

    implement sign up screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -13,86 +13,91 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TapGestureRecognizer _signUpScreen  ;
+  TapGestureRecognizer _signUpScreen;
 
   @override
   void initState() {
-    final mode1 =Provider.of<UiMode>(context , listen: false);
-    _signUpScreen = new TapGestureRecognizer()..onTap = (){
-     mode1.changeLoginMode();
-     print(mode1.loginMode);
-    } ;
+    final mode1 = Provider.of<UiMode>(context, listen: false);
+    _signUpScreen = new TapGestureRecognizer()
+      ..onTap = () {
+        mode1.changeLoginMode();
+      };
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    final mode = Provider.of<UiMode>(context);
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: screenWidth / 1.5,
-              color: Colors.white,
-              child: Stack(
-                children: <Widget>[
-                  LayoutBuilder(
-                    builder: (context, constrain) {
-                      final mode = Provider.of<UiMode>(context);
-                      double topCurveHeight = constrain.maxHeight;
-                      return Transform.scale(
-                        scale: 1.1,
-                        child: Container(
-                          height: topCurveHeight / 1.5,
-                          decoration: BoxDecoration(
-                              color: Colors.grey[800],
-                              borderRadius: BorderRadius.only(
-                                bottomLeft: Radius.circular(topCurveHeight),
-                                bottomRight: Radius.circular(topCurveHeight),
-                              )),
-                          child: Center(
-                            child: mode.loginMode ? Text(
-                              "تسجيل الدخول",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 22,
-                              ),
-                            ) :Text(
-                              "انشاء حساب",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 22,
-                              ),
-                            ) ,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                  Transform.translate(
-                    offset: Offset(screenWidth / 4 + screenWidth * .12,
-                        screenWidth / 6.5 + screenWidth * .16),
-                    child: CircleAvatar(
-                      radius: screenWidth * .14,
-                      backgroundColor: Colors.yellow,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          Center(
-                            child: Icon(
-                              Icons.perm_identity,
-                              color: Colors.white,
-                              size: screenWidth * .14,
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: screenWidth / 1.5,
+                color: Colors.white,
+                child: Stack(
+                  children: <Widget>[
+                    LayoutBuilder(
+                      builder: (context, constrain) {
+                        double topCurveHeight = constrain.maxHeight;
+                        return Transform.scale(
+                          scale: 1.1,
+                          child: Container(
+                            height: topCurveHeight / 1.5,
+                            decoration: BoxDecoration(
+                                color: mode.loginMode
+                                    ? Colors.grey[800]
+                                    : Colors.yellow,
+                                borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(topCurveHeight),
+                                  bottomRight: Radius.circular(topCurveHeight),
+                                )),
+                            child: Center(
+                              child: mode.loginMode
+                                  ? Text(
+                                      "تسجيل الدخول",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                      ),
+                                    )
+                                  : Text(
+                                      "انشاء حساب",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 22,
+                                      ),
+                                    ),
                             ),
                           ),
-                        ],
+                        );
+                      },
+                    ),
+                    Transform.translate(
+                      offset: Offset(screenWidth / 4 + screenWidth * .12,
+                          screenWidth / 6.5 + screenWidth * .16),
+                      child: CircleAvatar(
+                        radius: screenWidth * .14,
+                        backgroundColor:
+                            mode.loginMode ? Colors.yellow : Colors.grey[800],
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Center(
+                              child: Icon(
+                                Icons.perm_identity,
+                                color: Colors.white,
+                                size: screenWidth * .14,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
 
-                  /*Transform.scale(
+                    /*Transform.scale(
                 scale: 1.5,
                 child: Transform.translate(
                   offset: Offset(0 , - screenWidth / 1.8),
@@ -136,120 +141,134 @@ class _LoginScreenState extends State<LoginScreen> {
                 child: Transform.translate(offset: Offset(0 , - screenWidth/1.88),
                 child: CircleAvatar(radius: screenWidth/8,),),
               )*/
-                ],
-              ),
-            ),
-            Stack(
-              children: <Widget>[
-                Positioned(
-                  child: Container(
-                    width: screenWidth / 2.5,
-                    height: screenWidth / 1.2,
-                    decoration: BoxDecoration(
-                        color: Colors.blue.withOpacity(0.3),
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(screenWidth),
-                          bottomRight: Radius.circular(screenWidth),
-                        )),
-                  ),
-                  bottom: screenWidth / 10,
-                  left: 0,
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 40),
-                        child: Container(
-                          height: screenWidth / 1.7,
-                          width: screenWidth,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black,
-                                blurRadius: 3,
-                                offset: Offset(1, 1),
+              ),
+              Stack(
+                children: <Widget>[
+                  Positioned(
+                    child: Container(
+                      width: screenWidth / 2.5,
+                      height: screenWidth / 1.2,
+                      decoration: BoxDecoration(
+                          color: Colors.blue.withOpacity(0.3),
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(screenWidth),
+                            bottomRight: Radius.circular(screenWidth),
+                          )),
+                    ),
+                    bottom: screenWidth / 10,
+                    left: 0,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: Column(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: Container(
+                            height: mode.loginMode
+                                ? screenWidth / 1.7
+                                : screenWidth / 1.2,
+                            width: screenWidth,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black,
+                                  blurRadius: 3,
+                                  offset: Offset(1, 1),
+                                ),
+                              ],
+                            ),
+                            child: Directionality(
+                              textDirection: TextDirection.rtl,
+                              child: Form(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 25,
+                                    vertical: 10,
+                                  ),
+                                  child: loginUiBox(
+                                    screenWidth: screenWidth,
+                                    mode: mode.loginMode,
+                                  ),
+                                ),
                               ),
-                            ],
+                            ),
                           ),
-                          child: Directionality(
-                            textDirection: TextDirection.rtl,
-                            child: Form(
+                        ),
+                        SizedBox(
+                          height: mode.loginMode ? 15 : 0,
+                        ),
+                        mode.loginMode
+                            ? Text(
+                                "هل نسيت كلمة السر ؟",
+                                style: TextStyle(fontSize: 18),
+                              )
+                            : SizedBox(
+                                height: 0,
+                                width: 0,
+                              ),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 20),
+                          child: ButtonTheme(
+                            buttonColor: Colors.blue[300],
+                            child: RaisedButton(
+                              onPressed: () {},
                               child: Padding(
                                 padding: const EdgeInsets.symmetric(
-                                  horizontal: 25,
-                                  vertical: 10,
+                                    horizontal: 40, vertical: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.arrow_back,
+                                      color: Colors.white,
+                                    ),
+                                    Text(
+                                      mode.loginMode
+                                          ? "تسجيل الدخول"
+                                          : "انشاء حساب",
+                                      style: TextStyle(
+                                          fontSize: 20, color: Colors.white),
+                                    ),
+                                  ],
                                 ),
-                                child: loginUiBox(screenWidth: screenWidth),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: 15,),
-                      Text("هل نسيت كلمة السر ؟" ,
-                      style: TextStyle(
-                        fontSize: 18
-                      ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 20),
-                        color: Colors.blue[300],
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 40 , vertical: 5),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Icon(Icons.arrow_back , color: Colors.white,),
-                              FlatButton(
-                                onPressed: (){},
-                                child: Text("تسجيل الدخول" ,
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white
-                                ),
-                                ),
+                        RichText(
+                          text: TextSpan(
+                            style: TextStyle(
+                                fontSize: 17,
+                                color: Colors.blue,
+                                fontFamily: "Cairo"),
+                            children: [
+                              TextSpan(
+                                text: mode.loginMode
+                                    ? "في حالة ليس لديك حساب يمكنك  "
+                                    : "في حاله ان لديك حساب  ",
+                                style: TextStyle(color: Colors.grey[800]),
                               ),
-
+                              TextSpan(
+                                  text: mode.loginMode
+                                      ? "انشاء حساب من هنا"
+                                      : "سجل الدخول من هنا",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  recognizer: _signUpScreen),
                             ],
                           ),
                         ),
-                      ),
-                      RichText(
-                        text: TextSpan(
-                          style: TextStyle(
-                            fontSize: 17,
-                            color: Colors.blue,
-                            fontFamily: "Cairo"
-                          ),
-                          children: [
-                            TextSpan(
-                              text: "في حالة ليس لديك حساب يمكنك  " ,
-                              style: TextStyle(color: Colors.grey[800])
-                            ),
-                            TextSpan(
-
-                                text: "انشاء حساب من هنا" ,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              recognizer: _signUpScreen
-                            )
-
-                          ]
-                        ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      )
-    );
+                ],
+              ),
+            ],
+          ),
+        ));
   }
 }
-
-
--- lib/widget/login_ui_box.dart
+++ lib/widget/login_ui_box.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:mobilestore/models/categories_ofline.dart';
+import 'package:mobilestore/providers/ui_mode.dart';
+import 'package:provider/provider.dart';
 import 'custom_text_form_feild.dart';
 
 class loginUiBox extends StatelessWidget {
   const loginUiBox({
     Key key,
     @required this.screenWidth,
+    @required this.mode
   }) : super(key: key);
 
   final double screenWidth;
+  final bool mode ;
+
 
   @override
   Widget build(BuildContext context) {
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -46,6 +52,23 @@ class loginUiBox extends StatelessWidget {
           hintText: "ادخل كلمة المرور هنا",
           icon: Icons.lock,
         ),
+        mode == false ? SizedBox(
+          height: screenWidth / 35,
+        ) : Container(),
+        mode == false ? Text(
+          "تاكيد الرقم السري",
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 18,
+          ),
+        ) : Container(),
+        mode == false ? SizedBox(
+          height: screenWidth / 35,
+        ): Container(),
+        mode == false ? CustomTextFormFiled(
+          hintText: "ادخل كلمة المرور هنا",
+          icon: Icons.lock,
+        ) : Container(),
       ],
     );
   }
